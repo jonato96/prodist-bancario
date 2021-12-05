943f6c43eb5223d1cd6106393c246e72bdb387f8
Jonathan <jonato96s@gmail.com>
2021-12-04 21:39:40 -0500

    Ejercicio para entender la teoria

---
+++ src/main/java/com/programacion/Principal.java
@@ -0,0 +1,14 @@
+package com.programacion;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        TransaccionBancaria tr = new TransaccionBancaria();
+        tr.realizarTranferencia("0930146", "314600",100.00f);
+        tr.realizarTranferencia("0930146", "314600",100.00f);
+        tr.realizarTranferencia("0930146", "314600",100.00f);
+
+    }
+
+}
